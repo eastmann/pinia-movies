3dab33868dbc39cb6eae2f7b579cf1c48ffdf302
Maxim Klochkov <maxim.klochkov@gmail.com>
2023-05-17 00:48:46 +0300

    Add '@' alias to Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  server: {
-    proxy: 'http://localhost:38080'
-  }
+
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
+
 })
